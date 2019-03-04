6002344c5fc4bb492fcfef086f546c99173004f3
920 <gmldud628@gmail.com>
2019-03-05 00:11:55 +0900

    deploy test

--- router/login/index.js
+++ router/login/index.js
@@ -36,26 +36,7 @@ router.post('/sign', (req, res) => {
 
   let {userId, userEmail, userPhone, password} = req.body.form
 
-  //promise
-  const changePassword = () => {
-    return new Promise((resolve, reject) => {
-      //비밀번호 암호화를 위해 사용
-      crypto.randomBytes(64, (err, buf) => {
-        const salt = buf.toString('base64')
-        console.log('salt = ', salt)
-        
-        crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
-          console.log('password = ', key.toString('base64'))
-          password = key.toString('base64')
-          resolve(password)
-        })
-      })
-    })
-  }
-
-  changePassword()
-    .then(resPwd => {
-      connection.query(`INSERT INTO DB_MBR
+  connection.query(`INSERT INTO DB_MBR
                         (
                             LOGIN_ID
                           , LOGIN_PW
@@ -69,7 +50,7 @@ router.post('/sign', (req, res) => {
                           , ?
                           , ?
                         )`
-                        , [userId, resPwd, userEmail, userPhone], (err, rows) => {
+                        , [userId, password, userEmail, userPhone], (err, rows) => {
 
                           console.log('==== 쿼리 날린후 ====')
                           if(err) return res.status(401).json({err: '에러발생'})
@@ -85,7 +66,57 @@ router.post('/sign', (req, res) => {
                             res.end(JSON.stringify(resData))
                           }
                         })
-                      })
+
+  //promise
+  // const changePassword = () => {
+  //   return new Promise((resolve, reject) => {
+  //     //비밀번호 암호화를 위해 사용
+  //     crypto.randomBytes(64, (err, buf) => {
+  //       const salt = buf.toString('base64')
+  //       console.log('salt = ', salt)
+        
+  //       crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
+  //         console.log('password = ', key.toString('base64'))
+  //         password = key.toString('base64')
+  //         resolve(password)
+  //       })
+  //     })
+  //   })
+  // }
+
+  // changePassword()
+  //   .then(resPwd => {
+  //     connection.query(`INSERT INTO DB_MBR
+  //                       (
+  //                           LOGIN_ID
+  //                         , LOGIN_PW
+  //                         , USEREMAIL
+  //                         , USERPHONE
+  //                       )
+  //                       VALUES
+  //                       (
+  //                           ?
+  //                         , ?
+  //                         , ?
+  //                         , ?
+  //                       )`
+  //                       , [userId, resPwd, userEmail, userPhone], (err, rows) => {
+
+  //                         console.log('==== 쿼리 날린후 ====')
+  //                         if(err) return res.status(401).json({err: '에러발생'})
+  //                         console.log(rows)
+
+  //                         if(rows.affectedRows > 0) {
+
+  //                           const resData = {}
+
+  //                           resData.ok = true
+
+  //                           res.status(200)
+  //                           res.end(JSON.stringify(resData))
+  //                         }
+  //                       })
+  //                     })
 })
 
 module.exports = router
